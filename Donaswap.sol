65cab233e9deda97e5290ada5dfe29bf1c0bb2fd
Donaswap <85658707+DonaSwap@users.noreply.github.com>
2022-01-03 22:22:29 +0100

    Update Donaswap.sol

--- Donaswap.sol
+++ Donaswap.sol
@@ -720,7 +720,7 @@ contract DONASWAP is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         //For MAINNET: 0x10ED43C718714eb63d5aA57B78B54704E256024E
         //For TESTNET: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
          // Create a swap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
